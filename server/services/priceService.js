bc6594c488246e01b0c835a3e2e92d356ed4edd1
akaash2005 <akaashvp@gmail.com>
2025-05-24 15:21:07 +0530

    fix

--- server/services/priceService.js
+++ server/services/priceService.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'crypto';
 import { dbHelpers } from '../database.js';
 import { scrapeProductDetails } from './scraperService.js';
 
